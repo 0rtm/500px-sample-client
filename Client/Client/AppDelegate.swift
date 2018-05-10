96080a1e954978b7bdfce813e314656ba0f225e3
Artem Tselikov <ar.tselikov@gmail.com>
2018-05-09 20:22:45 -0400

    add initial files

---
+++ Client/Client/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Client
+//
+//  Created by Artem Tselikov on 2018-05-09.
+//  Copyright © 2018 Artem Tselikov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
